70619364a1e99f08ad299c3a2a11437832d9651b
madr1pman <78435919+madr1pman@users.noreply.github.com>
2021-02-02 23:28:49 +0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function () 
+{
+	let page = document.getElementById("page");
+	let style = document.getElementById("style");
+	page.addEventListener('click', function (){
+		window.location.href = "https://youtu.be/5QZcOugHQ6s";
+	});
+	style.addEventListener('click', function (){
+		document.body.style.backgroundColor = "pink";
+	});
+});
